e1e8441266909728846c9b69756d7e98a01a6317
Amish <actions@github.com>
2026-03-04 11:03:54 +0530

    added 50 power(x,n)

---
+++ Maths/50_Pow_x_n/50_Pow_x_n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long p = n;
+        double ans = 1;
+
+        if(p < 0){
+            x = 1/x;
+            p = -p;
+        }
+
+        while(p != 0){
+            if(p & 1) ans *= x;
+            x *= x;
+            p >>= 1;
+        }
+
+        return ans;
+    }
+};
